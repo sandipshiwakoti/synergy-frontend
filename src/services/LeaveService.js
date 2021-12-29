de1b2e50ef48c2bc271d488b7686939401670711
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2021-12-29 19:04:03 +0545

    Add missing dependencies

--- src/services/LeaveService.js
+++ src/services/LeaveService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/leaves";
+// const API_URL = "http://localhost:8080/api/leaves";
 const API_URL = "https://synergy-hrm-backend.herokuapp.com/api/leaves";
 
 class LeaveService {
